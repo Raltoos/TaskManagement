cbae0e19aa47091bf339564cff413995b5bb0087
Anuj <mailanujhere@gmail.com>
2024-04-22 12:44:58 +0530

    Add notes functionality

--- task-script.js
+++ task-script.js
@@ -1,14 +1,18 @@
 const taskReady = document.querySelector('.ready');
 const initTask = taskReady.querySelector('.card-column');
-const dialog = document.querySelector('dialog');
-const form = document.querySelector('form');
+const dialog1 = document.querySelector('.dialog1');
+const dialog2 = document.querySelector('.dialog2');
+const form1 = document.querySelector('.form1');
+const form2 = document.querySelector('.form2');
 
 const addButton = document.querySelector('.addTask');
+// const addNoteButton = document.querySelector('.card-head img');
 
 var dragSrcEl = null;
 const readyTaskCol = document.querySelector('.ready .card-column');
 
 let items;
+let notes;
 
 function handleDragStart(e) {
   this.style.opacity = '0.1';
@@ -52,7 +56,7 @@ function handleDrop(e) {
   // Check if the drop target is the 'addTask' element
   if (addTaskText == e.target) {
     targetColumn.insertBefore(dragSrcEl, addTaskText);
-   }
+  }
   else {
     // If not, insert the task before the target element
     // targetColumn.insertBefore(dragSrcEl, e.target);
@@ -91,6 +95,19 @@ function reCheck() {
     addArea.addEventListener('drop', handleDrop, false);
     // addArea.addEventListener('dragend', handleDragEnd, false);
   });
+
+  notes = document.querySelectorAll('.card-note-list .new');
+  notes.forEach(li => {
+    const div = li.querySelector('div');
+    div.classList.add('click');
+
+    function handleClick() {
+      div.classList.toggle('click');
+    }
+
+    li.addEventListener('click', handleClick);
+    li.classList.remove('new');
+  });
 }
 
 function createTask(heading, description) {
@@ -98,14 +115,32 @@ function createTask(heading, description) {
   card.className = 'card';
   card.setAttribute('draggable', 'true');
 
-  const taskHeading = document.createElement('h4');
-  const taskDescription = document.createElement('p');
+  const cardHeadDiv = document.createElement("div");
+  cardHeadDiv.className = "card-head";
 
+  const taskHeading = document.createElement('h4');
   taskHeading.textContent = heading;
+  const img = document.createElement("img");
+  img.setAttribute("src", "add-post.png");
+
+  img.addEventListener('click', (e) => {
+    parentCard = e.target.parentNode.parentNode;
+    dialog2.showModal();
+  });
+
+  cardHeadDiv.appendChild(taskHeading);
+  cardHeadDiv.appendChild(img);
+
+  const taskDescription = document.createElement('p');
   taskDescription.textContent = description;
 
-  card.appendChild(taskHeading);
+  const ul = document.createElement("ul");
+  ul.className = "card-note-list";
+
+  // Append elements to cardDiv
+  card.appendChild(cardHeadDiv);
   card.appendChild(taskDescription);
+  card.appendChild(ul);
 
   const addText = initTask.querySelector('.addTask');
   initTask.insertBefore(card, addText);
@@ -113,15 +148,44 @@ function createTask(heading, description) {
 }
 
 addButton.addEventListener('click', () => {
-  dialog.showModal();
+  dialog1.showModal();
 });
 
-form.addEventListener('submit', (e) => {
+form1.addEventListener('submit', (e) => {
   e.preventDefault();
   var name = document.getElementById('taskName');
   var desc = document.getElementById('taskDescription');
   createTask(name.value, desc.value);
-  dialog.close();
+  dialog1.close();
+});
+
+function createNote(card, title, note) {
+  const noteElement = document.createElement('li');
+
+  // const list = document.createElement('ul');
+  const listTitle = document.createElement('h4');
+  listTitle.textContent = title;
+
+  const para = document.createElement('div');
+  para.textContent = note;
+
+  noteElement.appendChild(listTitle);
+  noteElement.appendChild(para);
+  noteElement.classList.add("new");
+
+  card.querySelector('.card-note-list').appendChild(noteElement);
+  reCheck();
+}
+
+let parentCard = undefined;
+
+form2.addEventListener('submit', (e) => {
+  e.preventDefault();
+  var name = document.getElementById('noteTitle');
+  var desc = document.getElementById('noteDescription');
+  createNote(parentCard, name.value, desc.value);
+  dialog2.close();
 });
 
 reCheck();
+
